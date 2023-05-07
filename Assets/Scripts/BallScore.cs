082ca053eb06f63f7fce6f68bfe53994dafe3075
JordanNewkirk <jordannewkirk@gmail.com>
2023-05-07 16:49:04 -0700

    Working on environment and also score system
    Environment update as well as a first push of the scoring system and how the balls spawn. The OnTriggerEnter is still a little buggy and I will be working on it more tonight. Once that is fixed though everything should be good for the scoring system and with balls spawning too

---
+++ Assets/Scripts/BallScore.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BallScore : MonoBehaviour
+{
+    public UnityEvent<int> Scored;
+  
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("ScoringArea"))
+        {
+            ScoreArea scoreArea = other.GetComponent<ScoreArea>();
+            Scored.Invoke(scoreArea.GetPoints());
+        }
+
+    }
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+    public int startingBalls = 10;
+    public GameObject ballPrefab;
+    public Transform ballSpawn;
+    public TMP_Text scoreText;
+    public TMP_Text ballsRemainingText;
+
+    private int score = 0;
+    private int ballsRemaining;
+    private BallScore ballScore;
+
+    private void Start()
+    {
+        ballsRemaining = startingBalls;
+        UpdateBallsRemaining();
+
+        ballScore = ballPrefab.GetComponent<BallScore>();
+        ballScore.Scored.AddListener(UpdateScore);
+    }
+
+    void UpdateScore(int points)
+    {
+        score += points;
+        UpdateBallsRemaining();
+    }
+
+    void UpdateBallsRemaining()
+    {
+        ballsRemaining--;
+        UpdateUI();
+
+        if(ballsRemaining > 0)
+        {
+            SpawnBall();
+        }
+    }
+
+    void SpawnBall()
+    {
+        Instantiate(ballPrefab, ballSpawn.position, Quaternion.identity);
+    }
+
+    void UpdateUI()
+    {
+        scoreText.text = "Score: " + score;
+        ballsRemainingText.text = "Balls Remaining: " + ballsRemaining;
+    }
+}
